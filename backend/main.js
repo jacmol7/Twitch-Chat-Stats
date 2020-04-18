85eca4f2fd9a9b1bce67d0e7f52314b2611ee5ac
BuildTools <unconfigured@null.spigotmc.org>
2020-04-18 22:38:29 +0100

    Basic word and emote counting

---
+++ backend/main.js
@@ -0,0 +1,15 @@
+const MongoClient = require('mongodb').MongoClient;
+const twitchLogging = require('./twitchLogging.js');
+const url = 'mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb';
+
+MongoClient.connect(url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (err, client) => {
+    if(err) {
+        console.error(err);
+        return;
+    }
+
+    twitchLogging.start(client);
+  });
---
+++ backend/twitchLogging.js
@@ -0,0 +1,90 @@
+const tmi = require('tmi.js');
+const chalk = require('chalk');
+const fs = require('fs');
+
+var wordCollection;
+var client;
+const options = JSON.parse(fs.readFileSync('twitchSettings.json'));
+
+exports.start = (database) => {
+    wordCollection = database.db('twitch').collection('word');
+    client = new tmi.client(options);
+    client.on('message', onMessageHandler);
+    client.on('connected', onConnectedHandler);
+    client.connect();
+}
+
+function onMessageHandler(channel, user, msg, self) {
+    //stop if the message is from the bot
+    if (self) { return; }
+
+    //remove # at start of channel name
+    channel = channel.substring(1);
+
+    //count up all emotes
+    const emoteCounts = new Map();
+    if(user.emotes) {
+        for (let [emoteID, positions] of Object.entries(user.emotes)) {
+            for(let position of positions) {
+                let emoteRange = position.split('-');
+                let emoteName = msg.substr(emoteRange[0], emoteRange[1]-emoteRange[0]+1)
+                if(emoteCounts.has(emoteName)) {
+                    emoteCounts.get(emoteName).count += 1;
+                } else {
+                    emoteData = {
+                        emoteID: emoteID,
+                        count: 1
+                    }
+                    emoteCounts.set(emoteName, emoteData);
+                }
+            }
+        }
+    }
+
+    // Count up all words
+    const words = msg.trim().split(' ');
+    let wordCounts = new Map();
+    words.forEach(word => {
+        if(!wordCounts.has(word)) {
+            wordCounts.set(word, 1);
+        } else {
+            wordCounts.set(word, wordCounts.get(word)+1);
+        }
+    });
+
+    // write to database
+    for (let word of wordCounts.keys()) {
+        wordCollection.findOne({_id:word}).then(document => {
+            //console.log(document);
+            if (document) {
+                if(document.streamer[channel]) {
+                    document.streamer[channel] += 1;
+                } else {
+                    document.streamer[channel] = 1;
+                }
+                wordCollection.replaceOne({_id:word}, document);
+            } else {
+                let newDocument = {
+                    _id: word,
+                    word: word,
+                    streamer: {},
+                    isEmote: false
+                }
+                newDocument.streamer[channel] = 1
+                if(emoteCounts.has(word)) {
+                    newDocument.isEmote = true
+                    newDocument.emoteID = emoteCounts.get(word).emoteID;
+                }
+                wordCollection.insertOne(newDocument);
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+
+    console.log(`${chalk.hex(user.color || '#FFFFFF')(user.username)} : ${msg.trim()}`);
+}
+
+function onConnectedHandler(address, port) {
+    console.log(`Connected to ${address}:${port}`);
+}
