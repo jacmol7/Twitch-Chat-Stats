461f7c176ce07cb8bf2f8ce894707a114d2bdd80
BuildTools <unconfigured@null.spigotmc.org>
2020-04-19 19:30:04 +0100

    Added top emote for streamer

--- backend/resultsApi.js
+++ backend/resultsApi.js
@@ -3,11 +3,6 @@ const app = express();
 const port = 3000;
 var wordCollection;
 
-app.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello World!');
-});
-
 /* app.get('*', (req, res) => {
     console.log(req.query.test);
     res.send('wildcard');
@@ -26,7 +21,6 @@ app.get('/topword', (req, res) => {
         return;
     }
     const streamerIndex = "streamer."+streamer;
-    console.log(streamer);
 
     // retrieve number of results requested from get params
     let max = req.query.max;
@@ -39,7 +33,6 @@ app.get('/topword', (req, res) => {
         });
         return;
     }
-    console.log(max);
     
     let query = {isEmote: {"$eq": false}, word: {"$exists": true}};
     query[streamerIndex] = {"$exists": true}
@@ -56,9 +49,46 @@ app.get('/topword', (req, res) => {
     
 });
 
-// top emote
-
 // top emote for streamer
+app.get('/topemote', (req, res) => {
+    // retrieve name of streamer from get params
+    const streamer = req.query.streamer;
+    if (!streamer) {
+        res.send({
+            error: 'No streamer was specified'
+        });
+        return;
+    }
+    const streamerIndex = "streamer."+streamer;
+
+    // retrieve number of results requested from get params
+    let max = req.query.max;
+    if (!max) {
+        max = 1;
+    }
+    if (isNaN(max)) {
+        res.send({
+            error: "Max count was not a number"
+        });
+        return;
+    }
+    
+    let query = {isEmote: {"$eq": true}, word: {"$exists": true}, emoteID: {"$exists": true}};
+    query[streamerIndex] = {"$exists": true}
+    
+    let sort = {};
+    sort[streamerIndex] = -1;
+    
+    let filter = {projection: {_id: 0, word: 1, emoteID: 1}};
+    filter.projection[streamerIndex] = 1
+
+    wordCollection.find(query, filter).sort(sort).toArray().then(result => {
+        res.send(result.slice(0,max));
+    });
+    
+});
+
+// top emote
 
 exports.start = client => {
     wordCollection = client.db('twitch').collection('word');
