574d599fd2841dbe225da416caf49e3f0a245ff8
BuildTools <unconfigured@null.spigotmc.org>
2020-04-28 21:24:01 +0100

    Add retry attempts to database connection

--- backend/main.js
+++ backend/main.js
@@ -6,15 +6,26 @@ const fs = require('fs');
 const dbOptions = JSON.parse(fs.readFileSync('mongodbSettings.json'));
 const url = `mongodb://${dbOptions.username}:${dbOptions.password}@jacmol.com:27017/?compressors=zlib`;
 
-MongoClient.connect(url,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }, (err, client) => {
-    if(err) {
-        console.error(err);
-        return;
-    }
+let success = false;
+let retries = 5;
+while (!success && retries > 0) {
+  try {
+    MongoClient.connect(url,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }, (err, client) => {
+      if(err) {
+          console.error(err);
+          return;
+      }
 
-    twitchLogging.start(client);
-    resultsApi.start(client);
-  });
+      twitchLogging.start(client);
+      resultsApi.start(client);
+      success = true;
+    });
+  }
+  catch (err) {
+    console.error(err);
+  }
+  retries--;
+}
