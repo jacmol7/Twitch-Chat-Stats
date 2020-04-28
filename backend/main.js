561e21eac144bfbc8232ee9acbc23e78cf5f0f2d
BuildTools <unconfigured@null.spigotmc.org>
2020-04-28 21:39:42 +0100

    Removed broken retry idea

--- backend/main.js
+++ backend/main.js
@@ -4,29 +4,19 @@ const resultsApi = require('./resultsApi.js');
 const fs = require('fs');
 
 const dbOptions = JSON.parse(fs.readFileSync('mongodbSettings.json'));
-const url = `mongodb://${dbOptions.username}:${dbOptions.password}@0.0.0.0:27017/?compressors=zlib`;
+const url = `mongodb://${dbOptions.username}:${dbOptions.password}@jacmol.com:27017/?compressors=zlib`;
 console.log(url);
 
-let success = false;
-let retries = 5;
-while (!success && retries > 0) {
-  try {
-    MongoClient.connect(url,{
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }, (err, client) => {
-      if(err) {
-          console.error(err);
-          return;
-      }
+MongoClient.connect(url,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err, client) => {
+  if(err) {
+      console.error(err);
+      return;
+  }
 
-      twitchLogging.start(client);
-      resultsApi.start(client);
-      success = true;
-    });
-  }
-  catch (err) {
-    console.error(err);
-  }
-  retries--;
-}
+  twitchLogging.start(client);
+  resultsApi.start(client);
+  success = true;
+});
