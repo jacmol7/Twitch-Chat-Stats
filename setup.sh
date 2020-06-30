8531182ef7450ee19cf47fc2e8ac80bd64b2f7f6
Jacob <a>
2020-06-30 15:43:00 +0100

    Copy env file

--- setup.sh
+++ setup.sh
@@ -4,5 +4,8 @@ git clone https://github.com/jacmol7/Twitch-Chat-Stats
 echo "make start script executable"
 chmod +x Twitch-Chat-Stats/backend/start.sh
 
+echo "copy env file"
+cp .env /Twitch-Chat-Stats/backend
+
 echo "run start script"
-(cd Twitch-Chat-Stats/backend && sh ./Twitch-Chat-Stats/backend/start.sh)
+(cd Twitch-Chat-Stats/backend && sh ./start.sh)
